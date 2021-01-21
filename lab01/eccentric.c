81ec3a9675c4813c23ca6bad85a57927df996d31
JustinYokota <jyokota@berkeley.edu>
2021-01-20 18:35:55 -0800

    Added Lab01 Starter
    * Added lab01
    
    * Fixed char
    
    * Update .gitignore
    
    * Added onepercent
    
    Co-authored-by: Justin Yokota <>

---
+++ lab01/eccentric.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/* Only change any of these 4 values */
+#define V0 0
+#define V1 -1
+#define V2 0
+#define V3 0
+
+//Do not modify anything below this line!
+int main(void) {
+    int a;
+    char *s;
+
+    /* This is a print statement. Notice the little 'f' at the end!
+     * It might be worthwhile to look up how printf works for your future
+     * debugging needs... */
+    printf("Berkeley eccentrics:\n");
+	/* This line works the same way as printf; more on this in exercise 4*/
+	fprintf(stdout,"====================\n");
+
+    /* for loop */
+    for (a = 0; a < V0; a++)
+    {
+        printf("Happy ");
+    }
+    printf("\n");
+
+    /* switch statement */
+    switch (V1)
+    {
+    case 0:
+        printf("Yoshua\n");
+    case 1:
+        printf("Triangle Man\n");
+        break;
+    case 2:
+        printf("Chinese Erhu Guy\n");
+    case 3:
+	case 4:
+        printf("Yoshua\n");
+        break;
+    case 5:
+        printf("Dr. Jokemon\n");
+        break;
+    case 6:
+        printf("Hat Lady\n");
+    default:
+        printf("I don't know these people!\n");
+    }
+
+    /* ternary operator */
+    s = (V3 == 3) ? "Go" : "Boo";
+
+    /* if statement */
+    if (V2) {
+        printf("%s BEARS!\n", s);
+    } else {
+        printf("%s CARDINAL!\n", s);
+    }
+
+    return 0;
+}
---
+++ lab01/primes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAXVAL 10000 //Actually 1% of max value, to make percent messages easier.
+//Returns 1 if the input is a prime number, and 0 otherwise.
+int isPrime(int n)
+{
+	if(n%2 == 0) return 0;
+	for(int i = 3; i < n; i+=2)
+	{
+		if(n%i == 0) return 0;
+	}
+	return 1;
+}
+
+int main(int argc, char *argv[]) {
+    /*Allows for an optional command line input. This is only for autograder testing of your code; you should not use the command line input for this exercise.*/
+	int onepercent = (argc == 2 ? atoi(argv[1]) : MAXVAL); 
+	/*You may assume that the above line is correct*/
+    int i = 2;
+	for(;i<onepercent*100;i++) //So that percent messages are easier to set up.
+	{
+		if(isPrime(i)) //Checks if a number is prime. As a reminder, 0 is false, and any nonzero value is true.
+		{
+			fprintf(stdout, "%d", i); //Prints the number i to standard output
+		}
+	}
+}
